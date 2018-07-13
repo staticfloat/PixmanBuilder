77d6b33277ff751d5b78b489711d142b35dd8477
staticfloat@gmail.com <staticfloat@gmail.com>
2018-07-13 02:44:41 +0000

    Update build script, enable all platforms, move over to Gitlab CI

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # PixmanBuilder
 
-[![Build Status](https://travis-ci.org/staticfloat/PixmanBuilder.svg?branch=master)](https://travis-ci.org/staticfloat/PixmanBuilder)
+[![Build Status](https://gitlab.com/BinaryBuilder.jl/PixmanBuilder/badges/master/pipeline.svg)](https://gitlab.com/BinaryBuilder.jl/PixmanBuilder/pipelines)
 
-This is an example repository showing how to construct a "builder" repository for a binary dependency.  Using a combination of [`BinaryBuilder.jl`](https://github.com/staticfloat/BinaryBuilder.jl), [Travis](https://travis-ci.org), and [GitHub releases](https://docs.travis-ci.com/user/deployment/releases/), we are able to create a fully-automated, github-hosted binary building and serving infrastructure.
+This is a builder repository for [Pixman](http://www.pixman.org/). Using a combination of [`BinaryBuilder.jl`](https://github.com/staticfloat/BinaryBuilder.jl), [Gitlab CI](https://about.gitlab.com/features/gitlab-ci-cd/), and [GitHub releases](https://docs.travis-ci.com/user/deployment/releases/), we are able to create a fully-automated, github-hosted binary building and serving infrastructure.
--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,9 +1,12 @@
 using BinaryBuilder
 
 # Collection of sources required to build Pixman
+name = "Pixman"
+version = v"0.34.0"
 sources = [
     "https://www.cairographics.org/releases/pixman-0.34.0.tar.gz" =>
     "21b6b249b51c6800dc9553b65106e1e37d0e25df942c90531d4c3997aa20a88e",
+    "patches",
 ]
 
 # Bash recipe for building across all platforms
@@ -12,23 +15,10 @@ cd $WORKSPACE/srcdir
 cd pixman-0.34.0/
 
 # Apply patch for compilation with clang
-patch <<END
-diff --git a/configure.ac b/configure.ac
-index e833e45..cbebc82 100644
---- a/configure.ac
-+++ b/configure.ac
-@@ -1101,7 +1101,7 @@ support_for_gcc_vector_extensions=no
- AC_MSG_CHECKING(for GCC vector extensions)
- AC_LINK_IFELSE([AC_LANG_SOURCE([[
- unsigned int __attribute__ ((vector_size(16))) e, a, b;
--int main (void) { e = a - ((b << 27) + (b >> (32 - 27))) + 1; return e[0]; }
-+int main (void) { __builtin_shuffle(a,b);e = a - ((b << 27) + (b >> (32 - 27))) + 1; return e[0]; }
- ]])], support_for_gcc_vector_extensions=yes)
- 
- if test x$support_for_gcc_vector_extensions = xyes; then
--- 
-2.10.0
-END
+patch < $WORKSPACE/srcdir/patches/clang.patch
+
+# Apply patch for arm on musl
+patch -p1 < $WORKSPACE/srcdir/patches/arm_musl.patch
 
 aclocal
 automake
@@ -40,16 +30,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    BinaryProvider.Linux(:i686, :glibc),
-    BinaryProvider.Linux(:x86_64, :glibc),
-    BinaryProvider.Linux(:aarch64, :glibc),
-    BinaryProvider.Linux(:armv7l, :glibc),
-    BinaryProvider.Linux(:powerpc64le, :glibc),
-    BinaryProvider.MacOS(),
-    BinaryProvider.Windows(:i686),
-    BinaryProvider.Windows(:x86_64)
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products(prefix) = [
@@ -61,4 +42,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "Pixman", sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
